88ac1626c8f1c09386eca0e25660c77a677866cc
Leyder Quintero <desarrollador9@wedevel.co>
2024-06-16 17:48:10 -0500

    Encriptar contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from "bcryptjs";
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
@@ -15,16 +18,20 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel( createUserDto );
-
-      //! Encriptar contraseña 
-
-      //! Guardar el usuario
-
-      //! Generar JWT
-
-      return await newUser.save();
+      const {password, ...userData} = createUserDto;
       
+      //* Encriptar contraseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      }); 
+
+      await newUser.save();
+
+      const { password:_, ...user } = newUser.toJSON();
+
+      return user;
+
     } catch (error) {
 
       if( error.code === 11000 ){
